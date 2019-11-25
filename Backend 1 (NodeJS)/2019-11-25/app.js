aaa21307c1a9ef9e9b43b3e9e6a549e62e64efa1
Stephan Ljungros <stephan.ljungros@gmail.com>
2019-11-25 14:38:06 +0100

    Added some more data

--- Backend 1 (NodeJS)/2019-11-25/app.js
+++ Backend 1 (NodeJS)/2019-11-25/app.js
@@ -6,18 +6,36 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+let users = [];
 io.on('connection', (socket) => {
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
+
+
+    socket.on('test', (username) => {
+        if (users.indexOf(username) != -1) {
+            let newArray = users.filter((value, index) => {
+                if (value == username) {
+                    return false;
+                }
+                return true;
+            });
+            users = newArray;
+        }
+        socket.broadcast.emit('chat message', `${username} has disconnected`);
+        socket.broadcast.emit('online users', users);
     });
 
     socket.on('chat message', msg => {
         socket.broadcast.emit('chat message', msg);
-        console.log('Message: ' + msg);
+        console.log(users);
     });
 
     socket.on('new user', username => {
+        if (users.indexOf(username) == -1) {
+            users.push(username);
+            console.log(users);
+        }
         socket.broadcast.emit('chat message', `User ${username} has joined the channel!`);
+        socket.broadcast.emit('online users', users);
     });
 
     socket.on('isTyping', msg => {
